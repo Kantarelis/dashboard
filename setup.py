ec678b7dafb45abae17fd44b96bff2cc110aa5e1
skantarelis <skantarelis@raymetrics.com>
2023-08-13 11:53:28 +0300

    implement setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="dashboard",
+    version="0.0.1",
+    description="A dashboard demo.",
+    long_description="README.md",
+    url="https://github.com/Kantarelis/dashboard",
+    author="Spyros Kantarelis",
+    author_email="spyrosscience@gmail.com",
+    install_requires=requirements,
+    include_package_data=True,
+    packages=[
+        "dashboard",
+    ],
+)
