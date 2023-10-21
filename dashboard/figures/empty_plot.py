83527eab030b43253aed25389ed991a9d1511b7c
skantarelis <skantarelis@raymetrics.com>
2023-10-21 14:43:15 +0300

    finished stock_candles.py and empty_plot.py

--- dashboard/figures/empty_plot.py
+++ dashboard/figures/empty_plot.py
@@ -1,7 +1,7 @@
 import plotly.graph_objects as go
 
 
-def empty_plot():
+def empty_plot() -> go.Figure:
     fig = go.Figure(
         data=[
             go.Candlestick(
--- dashboard/figures/stock_candles.py
+++ dashboard/figures/stock_candles.py
@@ -5,6 +5,7 @@ from multiprocessing.synchronize import Lock as LockType
 import plotly.graph_objects as go
 
 from dashboard.database.functions.generic import run_query
+from dashboard.figures.empty_plot import empty_plot
 from dashboard.settings import DATABASE_PATH
 
 CREATION_STOCKS_DATA_FEED_QUERY = """
@@ -14,17 +15,19 @@ CREATION_STOCKS_DATA_FEED_QUERY = """
                  """
 
 
-def stock_candles_figure(root_path: str, lock: LockType, stock_symbol: str):
+def stock_candles_figure(root_path: str, lock: LockType, stock_symbol: str) -> go.Figure:
+    # Gather results of selected stock.
     get_candle_stock_data = f"""
                 SELECT timestamp, open, high, low, close
                 FROM stocks_data_feed
                 WHERE symbol='{stock_symbol}'
                 """
-    db_location: str = os.path.abspath(os.path.join(root_path, DATABASE_PATH))
+    db_location = os.path.abspath(os.path.join(root_path, DATABASE_PATH))
     with lock:
         run_query(CREATION_STOCKS_DATA_FEED_QUERY, db_location)
         results = run_query(get_candle_stock_data, db_location)
 
+    # If there are any results create the plotly figure Candlestick plot.
     if results:
         timestamps, open, high, low, close = [[], [], [], [], []]
         for result in results:
@@ -47,4 +50,5 @@ def stock_candles_figure(root_path: str, lock: LockType, stock_symbol: str):
             ]
         )
         return fig
-    return None
+    # Else return an empty data plotly figure Candlestick plot.
+    return empty_plot()
