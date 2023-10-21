2281af62102abac9db63ccd159e2db9a108c7212
skantarelis <skantarelis@raymetrics.com>
2023-10-21 22:17:27 +0300

    finished init_page

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -1,19 +1,20 @@
-SELECTED_COUNTRY = "US"
-COMPANY_NEWS_DATE_FORMAT = "%Y-%m-%d"
+SELECTED_COUNTRY: str = "US"
+COMPANY_NEWS_DATE_FORMAT: str = "%Y-%m-%d"
 
 # ----- Database ------
 # Database relative location
-DATABASE_PATH = "dashboard/database/dashboard_database.db"
+DATABASE_PATH: str = "dashboard/database/dashboard_database.db"
 
 
 # Application Paths
-INIT_PAGE_PATH = "/"
-MAIN_PAGE_PATH = "/dashboard_main"
+INIT_PAGE_PATH: str = "/"
+MAIN_PAGE_PATH: str = "/dashboard_main"
+URL_ANIMATION: str = "https://assets9.lottiefiles.com/packages/lf20_YXD37q.json"
 
 # Settings for stocks data feed
-TIME_INTERVAL = 1  # Days
-DATA_FEED_WINDOW = 1  # Days
-TIME_INTEGRAL_OF_IN_INIT_PAGE = 1000  # Milliseconds
-N_INTEGRALS_IN_INIT_PAGE = 1
-TIME_INTEGRAL_OF_GRAPH_REFRESH_RATE = 1000  # Milliseconds
-GRAPH_REFRESH_RATE = 5
+TIME_INTERVAL: int = 1  # Days
+DATA_FEED_WINDOW: int = 1  # Days
+TIME_INTEGRAL_OF_IN_INIT_PAGE: int = 1000  # Milliseconds
+N_INTEGRALS_IN_INIT_PAGE: int = 2
+TIME_INTEGRAL_OF_GRAPH_REFRESH_RATE: int = 1000  # Milliseconds
+GRAPH_REFRESH_RATE: int = 5
