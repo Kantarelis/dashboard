e5633918768330039e96919d5bf7b9942e9fa89f
skantarelis <skantarelis@raymetrics.com>
2023-11-19 14:59:28 +0200

    implement put and call functions for bs

--- dashboard/engine/black_scholes.py
+++ dashboard/engine/black_scholes.py
@@ -17,7 +17,27 @@ class BlackScholes(Stock):
         y_axis = self._integral_formula(x_axis)
         return np.trapz(y_axis)
 
-    def solution(self, stock_price: float, strike_price: float) -> float:
+    def put_solution(self, stock_price: float, strike_price: float) -> float:
+        if self.volatility is None:
+            error_message = f"Please provide a value for stock volatility for stock: {self.symbol}"
+            logging.error(error_message)
+            raise Exception(error_message)
+
+        d_one = (
+            np.log(stock_price / strike_price)
+            + (self.rf_r + 0.5 * (self.volatility**2)) * (self.target_days / FULL_YEAR_DAYS)
+        ) / (self.volatility * (self.target_days / FULL_YEAR_DAYS) ** 0.5)
+
+        d_two = (
+            np.log(stock_price / strike_price)
+            + (self.rf_r - 0.5 * (self.volatility**2)) * (self.target_days / FULL_YEAR_DAYS)
+        ) / (self.volatility * (self.target_days / FULL_YEAR_DAYS) ** 0.5)
+
+        return strike_price * np.exp(-self.rf_r * (self.target_days / FULL_YEAR_DAYS)) * self._nd_formula(
+            -d_two
+        ) - stock_price * self._nd_formula(-d_one)
+
+    def call_solution(self, stock_price: float, strike_price: float) -> float:
         if self.volatility is None:
             error_message = f"Please provide a value for stock volatility for stock: {self.symbol}"
             logging.error(error_message)
