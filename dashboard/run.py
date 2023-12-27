383ceed56359c345ac5fe076febef74bc7e861e5
Kantarelis <kantareliss@gmail.com>
2023-12-27 09:55:24 +0200

    added docstring in run.py

--- dashboard/run.py
+++ dashboard/run.py
@@ -1,6 +1,5 @@
 import os
 from multiprocessing import Lock
-from multiprocessing.synchronize import Lock as LockType
 
 import dash
 import dash_bootstrap_components as dbc
@@ -27,10 +26,22 @@ from dashboard.settings import DATABASE_PATH
 
 class Dashboard:
     def __init__(self, app_title: str = "Dashboard"):
+        """A demo dashboard application. This mini project is implemented as a show-case-scenario. It is a fully
+        functional, yet minimal, application for automatic stock tracking. The user can easily select his/hers
+        stocks-to-watch from a pool of stocks (for the sake of this demo these stocks are only from US). The user can
+        watch his/hers stocks, which are listing as interactive buttons on the left side of the dashboard, on the main
+        area of the application with interactive line and candle plots. There is also an analysis button with which one
+        can see, which stock needs to be sold based to an algorithm. This decision-making algorithm that runs in the
+        background is a simple application of the well known Black-Scholes equation. If a stock has no data, then the
+        verdict is it be sold, due to the fact that, with no data the stock cannot be trusted.
+
+        Args:
+            app_title (str, optional): The name of the application. Defaults to "Dashboard".
+        """
         self.app_title = app_title
-        self.app: dash.Dash = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
-        self.root_path: str = os.getcwd()
-        self.lock: LockType = Lock()
+        self.app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
+        self.root_path = os.getcwd()
+        self.lock = Lock()
         configure_environment()
         create_connection(DATABASE_PATH)
 
